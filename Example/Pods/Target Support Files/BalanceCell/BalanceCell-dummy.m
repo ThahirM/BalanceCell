08788caca25a3924e9347a425eed5cc054b4ac1d
Thahir Maheen <thahirmaheen@icloud.com>
2017-01-16 12:46:54 +0400

    updates

---
+++ Example/Pods/Target Support Files/BalanceCell/BalanceCell-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BalanceCell : NSObject
+@end
+@implementation PodsDummy_BalanceCell
+@end
---
+++ Example/Pods/Target Support Files/Pods-BalanceCell_Tests/Pods-BalanceCell_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BalanceCell_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BalanceCell_Tests
+@end
